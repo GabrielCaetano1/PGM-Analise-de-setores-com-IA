60cdbe1f2d24d66c6b214250eafc01d0098e2737
gabrielcaetano1 <gabrielcaetano2319@gmail.com>
2025-04-02 23:01:50 -0300

    feat: Atualizações no design do front

--- frontend/servidor.js
+++ frontend/servidor.js
@@ -1,13 +1,26 @@
-import express from 'express'
-import dotenv from 'dotenv'
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
-dotenv.config();
+const porta = 3000;
 const app = express();
 app.use(express.json());
-app.use('./user')
 
-const porta = process.env.PORTA
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+app.use(express.static(path.join(__dirname, 'src')))
+
+app.get("/main", (req, res) => {
+    res.sendFile(__dirname, "src/main/index.html")
+})
+app.get("/home", (req, res) => {
+    res.sendFile(__dirname, "src/avaliacao/index2.html")
+})
+app.get("/contato", (req, res) => {
+    res.sendFile(__dirname, "src/contato/index3.html")
+})
 
 app.listen(porta, () => {
-    console.log('O servidor está rodando na porta: http://localhost: ' + porta);
+    console.log('O servidor está rodando na porta: http://localhost:' + porta);
 })
